c56cd020644a2600404926cc4034155a0db9306c
nicoleerlano <erlanonicole98@gmail.com>
2023-10-29 21:38:16 +0200

    Add Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
 import CourseGoal from "./components/CourseGoal.tsx";
+import goalsImg from "./assets/goals.jpg";
+import Header from "./components/Header.tsx";
+import { useState } from "react";
+
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    const newGoal: CourseGoal = {
+      id: Math.random(),
+      title: "Learn React + TS",
+      description: "From ground up",
+    };
+    setGoals((prevGoals) => {
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
-      <CourseGoal title="Learn React + TS">
-        Learn it from the ground-up
-      </CourseGoal>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
