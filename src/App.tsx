e352dd11c7ba370fcf85d10c94d3420486262905
nicoleerlano <erlanonicole98@gmail.com>
2023-10-29 21:09:07 +0200

    Add CourseGoal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import CourseGoal from "./components/CourseGoal.tsx";
+
 export default function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <CourseGoal title="Learn React + TS">
+        Learn it from the ground-up
+      </CourseGoal>
+    </main>
+  );
 }
