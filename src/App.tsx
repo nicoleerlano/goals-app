b47e0da189f2a14feea1f03cb2e76bd79095ee68
nicoleerlano <erlanonicole98@gmail.com>
2023-10-30 14:22:02 +0200

    Handling the NewGoal inputs

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ export type CourseGoal = {
 export default function App() {
   const [goals, setGoals] = useState<CourseGoal[]>([]);
 
-  function handleAddGoal() {
-    const newGoal: CourseGoal = {
-      id: Math.random(),
-      title: "Learn React + TS",
-      description: "From ground up",
-    };
+  function handleAddGoal(goal: string, summary: string) {
     setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: goal,
+        description: summary,
+      };
       return [...prevGoals, newGoal];
     });
   }
@@ -33,7 +33,7 @@ export default function App() {
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <NewGoal />
+      <NewGoal onAddGoal={handleAddGoal} />
       <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );
